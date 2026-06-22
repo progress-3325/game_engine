784e5d0f39de0cdce8bf15c422d8aff994b69920
Ema <progress.3325ml@gmail.com>
2026-06-22 21:03:10 +0300

    made the logging class use the time library instead of std::chrono self implementation

--- Frame-Work/src/logging.cpp
+++ Frame-Work/src/logging.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <chrono>
+#include "cs_time.h"
 #include <cstring>
 #include <fstream>
 #include <unordered_map>
@@ -33,32 +33,8 @@ namespace cs
             {L_CODE::C_DEBUG, "<DEBUG>"},
             {L_CODE::C_RESET, "<END_OF_STATEMENT"},
             {L_CODE::C_ERROR, "<ERROR>"},
-            {L_CODE::C_ERROR_CONTINUE, "<ERROR>"},
+            {L_CODE::C_ERROR_CONTINUE, "<ERROR>"},  
         };
-
-        template<class duration>
-        std::chrono::hh_mm_ss<duration> get_time()
-        {
-            auto now = std::chrono::system_clock::now();
-            auto midnight = std::chrono::floor<std::chrono::days>(now);
-
-            std::chrono::hh_mm_ss<duration> tod{std::chrono::duration_cast<duration>(now - midnight)};
-
-            return tod;
-        }
-
-        template<class duration>
-        std::string format_time(std::chrono::hh_mm_ss<duration> time)
-        {
-            std::string c = ":";
-            std::string ret = std::string("[") + std::to_string(time.hours().count()) + c +
-                std::to_string(time.minutes().count()) + c + std::to_string(time.seconds().count()) +
-                std::string("]");
-            
-            return ret;
-        }
-
-
     }
 
     using seconds = std::chrono::seconds;
@@ -69,10 +45,10 @@ namespace cs
 
         if (!debug && c == L_CODE::C_DEBUG) return;
 
-        std::string cout_msg = std::string(detail::color_codes.at(c)) + detail::format_time<seconds>(detail::get_time<seconds>()) + 
+        std::string cout_msg = std::string(detail::color_codes.at(c)) + Time::get_time_str() + 
              std::string(msg) + std::string(detail::color_codes.at(L_CODE::C_RESET));
 
-        std::string ss_msg = std::string(detail::string_codes.at(c)) + detail::format_time<seconds>(detail::get_time<seconds>()) + 
+        std::string ss_msg = std::string(detail::string_codes.at(c)) + Time::get_time_str() + 
              std::string(msg) + std::string(detail::string_codes.at(L_CODE::C_RESET));
 
         std::cout << cout_msg << std::endl;
@@ -97,10 +73,11 @@ namespace cs
 
     void logger::save()
     {
+        std::lock_guard<std::mutex> guard(log_mutex);
         if (save_location != NULL)
         {
             std::lock_guard<std::mutex> guard(log_mutex);
-            std::ofstream file(std::string(save_location) + detail::format_time<seconds>(detail::get_time<seconds>()) + std::string(".log"), 
+            std::ofstream file(std::string(save_location) + Time::get_time_str() + std::string(".log"), 
                 std::ios_base::out);
 
             file.write(logged.str().c_str(), strlen(logged.str().c_str()));
