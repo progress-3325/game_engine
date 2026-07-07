0bcf525920a14337648804f97513ebea0865c7b3
Ema <spiridonemanuel23@gmail.com>
2026-07-07 23:05:51 +0300

    Made the window class to initialize GLFW and create a window and key_callback function for the event dispatching system

---
+++ Frame-Work/include/cs_window.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <GLFW/glfw3.h>
+#include "core.h"
+
+namespace cs
+{
+    class Window
+    {
+    public:
+        Window() = default;
+        Window(const uint32_t& p_width, const uint32_t& p_height, cstring p_title)
+            : width(p_width), height(p_height), title(p_title) 
+        {
+            glfwInit();
+            this->window = glfwCreateWindow(this->width, this->height, this->title, nullptr, nullptr);
+
+            glfwMakeContextCurrent(this->window);
+            glfwSetWindowUserPointer(this->window, this);
+        }
+
+        operator GLFWwindow*()
+        {
+            return this->window;
+        }
+    private:
+        GLFWwindow* window = nullptr;
+        uint32_t width{0}, height{0};
+        cstring title{'\0'};
+    };
+
+    void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+}
