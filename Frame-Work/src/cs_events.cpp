2275f55eca6020b26dba6d470fcfb27735302315
Ema <spiridonemanuel23@gmail.com>
2026-07-18 23:53:08 +0300

    added logging to execute functions, initializers for static variables in classes/structs and made the event getter more type safe

--- Frame-Work/src/cs_events.cpp
+++ Frame-Work/src/cs_events.cpp
@@ -11,13 +11,14 @@ static constexpr size_t mouse_button_amount = 8;
 
 namespace cs
 {
-    Event::~Event() = default;
+    Mutex EventDispatcher::m_lock{};
     namespace detail
     {
         struct ev_storer
         {
             static std::deque<std::unique_ptr<Event>> events;
         };
+        std::deque<std::unique_ptr<Event>> ev_storer::events{};
     }
 
     void EventDispatcher::dispatch(Event* ev)
@@ -35,23 +36,6 @@ namespace cs
         EventDispatcher::dispatch(this);
     }
 
-    void EventDispatcher::process()
-    {
-        Guard lock(m_lock);
-        
-        for (const std::unique_ptr<Event>& event : detail::ev_storer::events)
-        {
-            EventType ev_type(event->type());
-            if (ev_type.category == static_cast<uint16_t>(EventType::EventCategories::Application))
-            {
-                if (ev_type.type == static_cast<uint32_t>(EventType::ApplicationEventType::ApplicationCloseEvent))
-                {
-                    
-                }
-            }
-        }
-    }
-
     namespace detail
     {
         struct key_pressed
@@ -67,6 +51,19 @@ namespace cs
             static std::array<Time::stopwatch, mouse_button_amount> mouse_heldTime;
             static f64_t mouse_pos_x, mouse_pos_y, mouse_scroll_x, mouse_scroll_y;
         };
+
+        std::array<bool, key_amount> key_pressed::is_pressed{};
+        std::array<bool, key_amount> key_pressed::is_held{};
+        std::array<bool, key_amount> key_pressed::is_released{};
+        std::array<Time::stopwatch, key_amount> key_pressed::heldTime{};
+
+        std::array<bool, mouse_button_amount> key_pressed::mouse_is_pressed{};
+        std::array<bool, mouse_button_amount> key_pressed::mouse_is_held{};
+        std::array<bool, mouse_button_amount> key_pressed::mouse_is_released{};
+        std::array<Time::stopwatch, mouse_button_amount> key_pressed::mouse_heldTime{};
+        
+        
+
 
         f64_t key_pressed::mouse_pos_x{0.0f}, key_pressed::mouse_pos_y{0.0f}, key_pressed::mouse_scroll_x{0.0f}, key_pressed::mouse_scroll_y{0.0f};
 
@@ -97,7 +94,7 @@ namespace cs
         return detail::key_pressed::heldTime[static_cast<uint16_t>(key)].readMilliseconds();
     }
 
-    template<Input::MouseButtons buttons>
+    template<Input::MouseButtons button>
     bool isButtonPressed()
     {
         return detail::key_pressed::mouse_is_pressed[static_cast<uint16_t>(button)];
@@ -111,6 +108,8 @@ namespace cs
         detail::key_pressed::is_pressed[index] = true;
         detail::key_pressed::is_held[index] = true;
         detail::key_pressed::heldTime[index].start();
+
+        logger::log(this->name(), L_CODE::C_DEBUG);
     }
 
     void KeyReleasedEvent::execute() const
@@ -120,6 +119,8 @@ namespace cs
         detail::key_pressed::is_held[index] = false;
         detail::key_pressed::heldTime[index].stop();
         detail::key_pressed::is_released[index] = true;
+        logger::log(this->name(), L_CODE::C_DEBUG);
+        logger::log(std::string(std::string("Key pressed for ") + std::to_string(detail::key_pressed::heldTime[index].readMilliseconds())).c_str(), L_CODE::C_DEBUG);
     }
 
     void MouseButtonPressedEvent::execute() const
@@ -129,6 +130,7 @@ namespace cs
         detail::key_pressed::is_pressed[index] = true;
         detail::key_pressed::is_held[index] = true;
         detail::key_pressed::heldTime[index].start();
+        logger::log(this->name(), L_CODE::C_DEBUG);
     }
 
     void MouseButtonReleasedEvent::execute() const
@@ -138,23 +140,27 @@ namespace cs
         detail::key_pressed::is_held[index] = false;
         detail::key_pressed::is_released[index] = true;
         detail::key_pressed::heldTime[index].stop();
+        logger::log(this->name(), L_CODE::C_DEBUG);
     }
 
     void MouseMovedEvent::execute() const
     {
         detail::key_pressed::mouse_pos_x = this->X_axis;
         detail::key_pressed::mouse_pos_y = this->Y_axis;
+        logger::log(this->name(), L_CODE::C_DEBUG);
     }
 
     void MouseScrollEvent::execute() const
     {
         detail::key_pressed::mouse_scroll_x = this->position_x;
         detail::key_pressed::mouse_scroll_y = this->position_y;
+        logger::log(this->name(), L_CODE::C_DEBUG);
     }
 
 
     void EventDispatcher::process()
     {
+        Guard lock(m_lock);
         using namespace detail;
 
         key_pressed::is_pressed.fill(false);
@@ -162,7 +168,7 @@ namespace cs
 
         while (!ev_storer::events.empty())
         {
-            auto& event = ev_storer::events.front();
+            std::unique_ptr<Event> event = std::move(ev_storer::events.front());
             ev_storer::events.pop_front();
             event->execute();
         }
